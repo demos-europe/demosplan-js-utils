8ccdc7eedb8c5b8064a40b1a5736c91cf45a3f86
Florian Salis <salis@demos-deutschland.de>
2022-12-20 17:07:09 +0100

    chore: Update Test descriptions

--- tests/utils/changeUrlForPager.test.js
+++ tests/utils/changeUrlForPager.test.js
@@ -5,13 +5,13 @@ describe('changeUrlforPager', () => {
     global.window = Object.create(window);
   })
 
-  test('without previous Values', () => {
+  it('should set givven Values if there are none already set', () => {
     const url = changeUrlforPager({ count: 2, current_page: 1 })
 
     expect(url[1]).toBe("r_limit=2&page=1")
   })
 
-  test('with previous Values', () => {
+  it('should overwrite givven Values if there are values already set', () => {
     const url = "http://dummy.com?r_limit=2&page=5";
     Object.defineProperty(window, 'location', {
       value: {
@@ -24,7 +24,7 @@ describe('changeUrlforPager', () => {
     expect(updatedUrl[1]).toBe('r_limit=3&page=3')
   })
 
-  test('without previous limit', () => {
+  it('should overwrite givven limit if that is the only param', () => {
     const url = "http://dummy.com?r_limit=2";
     Object.defineProperty(window, 'location', {
       value: {
@@ -37,7 +37,7 @@ describe('changeUrlforPager', () => {
     expect(updatedUrl[1]).toBe('r_limit=3&page=3')
   })
 
-  test('without previous page', () => {
+  it('should overwrite givven pge if that is the only param', () => {
     const url = "http://dummy.com?page=2";
     Object.defineProperty(window, 'location', {
       value: {
@@ -50,7 +50,7 @@ describe('changeUrlforPager', () => {
     expect(updatedUrl[1]).toBe('r_limit=3&page=3')
   })
 
-  test('without previous page', () => {
+  it('should set params if they are set but undefined', () => {
     const url = "http://dummy.com?page=&r_limit=";
     Object.defineProperty(window, 'location', {
       value: {
@@ -61,5 +61,19 @@ describe('changeUrlforPager', () => {
     const updatedUrl = changeUrlforPager({ count: 3, current_page: 3 })
     expect(updatedUrl[0]).toBe('http://dummy.com')
     expect(updatedUrl[1]).toBe('r_limit=3&page=3')
+  })
+
+  it('should work even if there are other params otr the order ist mixed', () => {
+    const url = "http://dummy.com?test=tete&page=3&y=2&r_limit=3";
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: url
+      }
+    })
+
+    const updatedUrl = changeUrlforPager({ count: 1, current_page: 1 })
+    expect(updatedUrl[0]).toBe('http://dummy.com')
+    expect(updatedUrl[1]).toContain('r_limit=1')
+    expect(updatedUrl[1]).toContain('page=1')
   })
 })
