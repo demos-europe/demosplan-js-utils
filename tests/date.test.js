24909d735b95044464cad0343aeef3e5f2354745
Natalija Sakuta <sakuta@demos-deutschland.de>
2022-12-28 15:59:19 +0100

    refac(T30600): refactor code

--- tests/date.test.js
+++ tests/date.test.js
@@ -10,11 +10,11 @@ describe('formatDate function', () => {
         expect(formatDate('2019-09-25 13:15:40.000000', 'long')).toMatch('25.09.2019, 13:15 Uhr')
     })
 
-    it('transforms number to string using \'DD.MM.YYYY\' format', () => {
+    it('transforms number to a string using \'DD.MM.YYYY\' format', () => {
         expect(formatDate(1569369600000)).toMatch('25.09.2019')
     })
 
-    it('transforms number to string using a long \'DD.MM.YYYY, HH:mm [Uhr]\' format ', () => {
+    it('transforms number to a string using a long \'DD.MM.YYYY, HH:mm [Uhr]\' format ', () => {
         expect(formatDate(1569410140000, 'long')).toMatch('25.09.2019, 13:15 Uhr')
     })
 
--- tests/sortAlphabetically.test.js
+++ tests/sortAlphabetically.test.js
@@ -9,22 +9,26 @@ describe('sortAlphabetically function', () => {
 
     const mockArrayOfString = [ 'copySpec', 'email2', 'showlist', 'allowedRoleIds' ]
 
+    let filteredArray =  [
+        { id: 1031, firstName: 'Daniel', lastName: 'Ostermann' },
+        { id: 1035, firstName: 'Katharina', lastName: 'Maier' },
+        { id: 1067, firstName: 'Torsten', lastName: 'Wulf' }
+    ]
+
     it('returns an unchanged array if an object does not contain the given property', () => {
         expect(sortAlphabetically(mockUsers, 'phone')).toEqual(mockUsers)
     })
 
     it('sorts an object by the property "firstName" in alphabetical order', () => {
-        const filteredArray =  [
-            { id: 1031, firstName: 'Daniel', lastName: 'Ostermann' },
-            { id: 1035, firstName: 'Katharina', lastName: 'Maier' },
-            { id: 1067, firstName: 'Torsten', lastName: 'Wulf' }
-        ]
-
         expect(sortAlphabetically(mockUsers, 'firstName')).toEqual(filteredArray)
     })
 
-    it('sorts an object by the property "firstName" in reverse alphabetical order', () => {
-        const filteredArray =  [
+    it('sorts an object in alphabetical order by default', () => {
+        expect(sortAlphabetically(mockUsers, 'firstName')).toEqual(filteredArray)
+    })
+
+    it('sorts an object by the property "firstName" in reverse alphabetical order when "desc" parameter is provided', () => {
+        filteredArray =  [
             { id: 1067, firstName: 'Torsten', lastName: 'Wulf' },
             { id: 1035, firstName: 'Katharina', lastName: 'Maier' },
             { id: 1031, firstName: 'Daniel', lastName: 'Ostermann' }
@@ -33,25 +37,15 @@ describe('sortAlphabetically function', () => {
         expect(sortAlphabetically(mockUsers, 'firstName', 'desc')).toEqual(filteredArray)
     })
 
-    it('sorts an object by the property "lastName"', () => {
-        const newArrays =  [
-            { id: 1035, firstName: 'Katharina', lastName: 'Maier' },
-            { id: 1031, firstName: 'Daniel', lastName: 'Ostermann' },
-            { id: 1067, firstName: 'Torsten', lastName: 'Wulf' }
-        ]
-
-        expect(sortAlphabetically(mockUsers, 'lastName')).toEqual(newArrays)
-    })
-
     it('sorts an array of string in alphabetical order', () => {
         const sortedArrayOfString = mockArrayOfString.sort()
 
-        expect(sortAlphabetically(mockArrayOfString,'', 'asc')).toEqual(sortedArrayOfString)
+        expect(sortAlphabetically(mockArrayOfString, '', 'asc')).toEqual(sortedArrayOfString)
     })
 
     it('sorts an array of string in reverse alphabetical order', () => {
         const sortedArrayOfString = mockArrayOfString.sort().reverse()
 
-        expect(sortAlphabetically(mockArrayOfString,'', 'desc')).toEqual(sortedArrayOfString)
+        expect(sortAlphabetically(mockArrayOfString, '', 'desc')).toEqual(sortedArrayOfString)
     })
 })
