303c7a805ff80ac1c584728946bd1b3f08c65fa5
Natalija Sakuta <sakuta@demos-deutschland.de>
2022-12-29 16:15:35 +0100

    test(T30600): add tests for the toDate function, refactor some tests

--- tests/date.test.js
+++ tests/date.test.js
@@ -1,19 +1,24 @@
-import { formatDate } from '../utils/date'
+import {formatDate, toDate} from '../utils/date'
 import dayjs from 'dayjs'
 
-describe('formatDate', () => {
+describe('date', () => {
+    let dateFormat
+
     const date = {
         day: 25,
         month: 12,
         year: 2019,
-        time: '13:15:40.000000',
-        firstDay: '0' + 1
+        firstDay: '0' + 1,
+        time: {
+            opt1: '13:15:40.000000',
+            opt2: 'T12:15:40.000Z'
+        }
     }
 
     const dates = {
-        string: `${date.year}-${date.month}-${date.day} ${date.time}`,
+        string: `${date.year}-${date.month}-${date.day} ${date.time.opt1}`,
         number: 1577276140000,
-        date: `${date.year}-${date.month}-${date.day}T12:15:40.000Z`,
+        date: new Date(`${date.year}-${date.month}-${date.day}${date.time.opt2}`)
     }
 
     const transformedDates = {
@@ -25,43 +30,59 @@ describe('formatDate', () => {
     const currentDate = dayjs().format('DD.MM.YYYY')
 
     it('sets the short date format as a default value', () => {
-        expect(formatDate(dates.string)).toMatch(transformedDates.short1)
-        expect(formatDate(dates.number)).toMatch(transformedDates.short1)
-        expect(formatDate(dates.date)).toMatch(transformedDates.short1)
+        expect(formatDate(dates.string)).toEqual(transformedDates.short1)
+        expect(formatDate(dates.number)).toEqual(transformedDates.short1)
+        expect(formatDate(dates.date)).toEqual(transformedDates.short1)
     })
 
     it('sets the long date format if the "long" parameter is provided', () => {
-        expect(formatDate(dates.string, 'long')).toMatch(transformedDates.long)
-        expect(formatDate(dates.number, 'long')).toMatch(transformedDates.long)
-        expect(formatDate(dates.date, 'long')).toMatch(transformedDates.long)
+        expect(formatDate(dates.string, 'long')).toEqual(transformedDates.long)
+        expect(formatDate(dates.number, 'long')).toEqual(transformedDates.long)
+        expect(formatDate(dates.date, 'long')).toEqual(transformedDates.long)
     })
 
     it('transforms number to a string using \'DD.MM.YYYY\' format', () => {
-        expect(formatDate(dates.number)).toMatch(transformedDates.short1)
+        expect(formatDate(dates.number)).toEqual(transformedDates.short1)
     })
 
-    it('sets the \'DD.MM.YYYY\' date format if the provided date type is an instanceof date', () => {
-        expect(formatDate(dates.date)).toMatch(transformedDates.short1)
+    it('sets the \'DD.MM.YYYY\' date format if the provided date type is an instanceof Date', () => {
+        expect(formatDate(dates.date)).toEqual(transformedDates.short1)
     })
 
     it('sets the \'DD.MM.YYYY\' date format if the provided date type is a string', () => {
-        expect(formatDate(dates.string)).toMatch(transformedDates.short1)
+        expect(formatDate(dates.string)).toEqual(transformedDates.short1)
     })
 
     it('sets the current date if the provided date type is a null or undefined', () => {
-        expect(formatDate(null)).toMatch(currentDate)
-        expect(formatDate(undefined)).toMatch(currentDate)
+        expect(formatDate(null)).toEqual(currentDate)
+        expect(formatDate(undefined)).toEqual(currentDate)
     })
 
     it('transforms valid ISO 8601 date format types to DD.MM.YYYY format', () => {
-        const dateISOFormat = {
+        dateFormat = {
             date1: date.year + '-' + date.month + '-' + date.day,
             date2: date.year + '-' + date.month,
             date3: date.year + '/' + date.month
         }
 
-        expect(formatDate(dateISOFormat.date1)).toEqual(transformedDates.short1)
-        expect(formatDate(dateISOFormat.date2)).toEqual(transformedDates.short2)
-        expect(formatDate(dateISOFormat.date3)).toEqual(transformedDates.short2)
+        expect(formatDate(dateFormat.date1)).toEqual(transformedDates.short1)
+        expect(formatDate(dateFormat.date2)).toEqual(transformedDates.short2)
+        expect(formatDate(dateFormat.date3)).toEqual(transformedDates.short2)
+    })
+
+    it('transforms string to a Date', () => {
+        expect(toDate(dates.string)).toEqual(dates.date)
+    })
+
+    it('transforms number to a Date', () => {
+        expect(toDate(dates.number)).toEqual(dates.date)
+    })
+
+    it('transforms valid ISO 8601 date format type to a Date', () => {
+        dateFormat = {
+            date: date.year + '/' + date.month + '/' + date.day + date.time.opt1
+        }
+
+        expect(toDate(dateFormat.date)).toEqual(dates.date)
     })
 })
