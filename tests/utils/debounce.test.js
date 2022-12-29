f52fee3e5c61d5138e5e53c67e3366221488aab7
Lamia Gülsah Elmas <elmas@demos-deutschland.de>
2022-12-29 12:40:43 +0100

    feat: Code beautify

--- tests/utils/debounce.test.js
+++ tests/utils/debounce.test.js
@@ -1,4 +1,4 @@
-import { debounce } from "../../index";
+import { debounce } from "../../index"
 
 describe("debounce", () => {
     beforeEach(() => {
@@ -7,44 +7,44 @@ describe("debounce", () => {
         * This is replacing the original implementation of setTimeout() and other timer functions.
         * Timers can be restored to their normal behavior with jest.useRealTimers().
         */
-        jest.useFakeTimers();
+        jest.useFakeTimers()
     });
-    it("doesnt call the function if it has a waiting time", () => {
-        const callback = jest.fn();
-        const debounced = debounce(callback, 500);
-        debounced(callback);
-        expect(callback).not.toBeCalled();
+    it('doesnt call the function if it has a waiting time', () => {
+        const callback = jest.fn()
+        const debounced = debounce(callback, 500)
+        debounced(callback)
+        expect(callback).not.toBeCalled()
 
-        jest.advanceTimersByTime(100);
+        jest.advanceTimersByTime(100)
         debounced();
-        expect(callback).not.toBeCalled();
+        expect(callback).not.toBeCalled()
 
-        jest.advanceTimersByTime(499);
-        expect(callback).not.toBeCalled();
+        jest.advanceTimersByTime(499)
+        expect(callback).not.toBeCalled()
 
-        jest.advanceTimersByTime(300);
-        expect(callback).toBeCalledTimes(1);
+        jest.advanceTimersByTime(300)
+        expect(callback).toBeCalledTimes(1)
     });
 
-    it("fires the callback, when immediate is true, no matter of the waiting time", () => {
+    it('fires the callback, when immediate is true, no matter of the waiting time', () => {
         const callback = jest.fn();
-        const debounced = debounce(callback, 500, true);
-        expect(callback).not.toBeCalled();
-        debounced();
-        expect(callback).toBeCalledTimes(1);
+        const debounced = debounce(callback, 500, true)
+        expect(callback).not.toBeCalled()
+        debounced()
+        expect(callback).toBeCalledTimes(1)
 
-        jest.advanceTimersByTime(100);
-        debounced();
-        expect(callback).toBeCalledTimes(1);
+        jest.advanceTimersByTime(100)
+        debounced()
+        expect(callback).toBeCalledTimes(1)
 
-        jest.advanceTimersByTime(499);
-        expect(callback).toBeCalledTimes(1);
+        jest.advanceTimersByTime(499)
+        expect(callback).toBeCalledTimes(1)
 
-        jest.advanceTimersByTime(1);
-        expect(callback).toBeCalledTimes(1);
+        jest.advanceTimersByTime(1)
+        expect(callback).toBeCalledTimes(1)
 
-        jest.advanceTimersByTime(700);
-        debounced();
-        expect(callback).toBeCalledTimes(2);
+        jest.advanceTimersByTime(700)
+        debounced()
+        expect(callback).toBeCalledTimes(2)
     });
 });
