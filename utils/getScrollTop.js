ece10fc520caa6282a8ccdaa631fc79aa4f8605e
Lamia Gülsah Elmas <elmas@demos-deutschland.de>
2022-12-30 15:12:28 +0100

    feat: A test for getScrollTop.js

--- utils/getScrollTop.js
+++ utils/getScrollTop.js
@@ -3,6 +3,6 @@
  * @return {number}
  * @private
  */
-export default function getScrollTop () {
-  return Math.abs(parseInt(window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop, 10))
+export default function getScrollTop() {
+  return Math.abs((window.scrollY || window.scrollTop || document.getElementsByTagName('html')[0].scrollTop, 10))
 }
